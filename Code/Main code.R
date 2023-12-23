e52acff3304c4a77aeda7210bb75ada38b8f3371
Hsiang-Yang Gary Shyu <gary20040529@gmail.com>
2023-12-22 21:34:26 -0500

    Add project

---
+++ Code/Main code.R
@@ -0,0 +1,5 @@
+knitr::opts_chunk$set(echo = FALSE, fig.cap = TRUE, fig.align = 'center', warning = FALSE, message = FALSE, dpi = 600)
+pacman::p_load(plyr,dplyr,reshape2,stringr,lubridate,readxl,openxlsx,janitor,ggplot2,ggpmisc,ggpubr,ggsignif,gplots,data.table,magrittr,grid,
+               tidyverse,rstatix,PtProcess,gtable,gridExtra,cowplot,jpg,tiff,pwr,MOTE,kableExtra,tinytex,knitr,quantmod,class,caret,gmodels,
+               officedown,officer,flextable,car,C50,GGally,ggResidpanel,ggfortify,caretEnsemble,randomForest,corrplot,neuralnet,lemon,Hmisc,scales,
+               glue,ggtext,png,gtools,ggrepel,rvg,gdata,scales,nnet,xgboost,correlation,psych,corrgram,jpeg)
